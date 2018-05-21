095fbcc8342e65943c8ad64ba30ee6e7be66028d
MatheusNascimentoti99 <32804625+MatheusNascimentoti99@users.noreply.github.com>
2018-05-21 15:17:45 -0300

    Update App.java

--- src/view/App.java
+++ src/view/App.java
@@ -21,8 +21,7 @@ public class App {
     public static void main(String args[]) throws IOException, Exception {
         String escolha;
 
-        do {
-            Croupier croupier = new Croupier();
+        do {                                                                    //Laço para manter a execução do programa após o termino das principais operações do mesmo.            
             ControllerPartida controlePartida = new ControllerPartida();
             ControllerJogador controleUser = new ControllerJogador();
             System.out.println("Digite 1 para Cadastrar jogador ");
@@ -30,26 +29,26 @@ public class App {
             System.out.println("Digite 3 para mostrar placar");
             System.out.println("Digite 4 para sair do jogo");
             Partida partida;
-            Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);                         //Entrada do usuário
             escolha = input.next();
 
             switch (escolha) {
                 case "1":
-                    controleUser.cadastrar();
+                    controleUser.cadastrar();                               //Aba para novo cadastro
                     break;
                 case "2":
-                    partida = controlePartida.escolherPartida();
+                    partida = controlePartida.escolherPartida();            //Aba para nova partida
                     partida.partida(controleUser);
                     break;
                 case "3":
-                    controleUser.mostrarJogadores();
+                    controleUser.mostrarJogadores();                        //Aba para mostrar placar
                 default:
                     break;
             }
             System.out.println("");
-            controleUser.getControleFile().imprimirPontuacao(controleUser.getListaJogadores());
-            controleUser.getControleFile().salvarJogador(controleUser.getListaJogadores());
-        } while (!escolha.equals("4"));
+            controleUser.getControleFile().imprimirPontuacao(controleUser.getListaJogadores());     //Atualiza o rank de pontuação ao fim de qualquer execução do programa
+            controleUser.getControleFile().salvarJogador(controleUser.getListaJogadores());         //Atualiza os dados dos jogadores ao fim de qualquer execução do programa
+        } while (!escolha.equals("4"));                                                             //Executa o programa até o usuário digitar 4 entre as opções do menu principal
 
     }
 }
